e661c5cc31ca0381a5d60a93620935b727b4e7e7
Adreynd <jackimball@yahoo.com>
2018-06-04 00:15:22 -0700

    First draft of changes

---
+++ CustomerMaintenanceStart/CustomerMaintenanceClasses/CustomerList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomerMaintenanceClasses
+{
+    class CustomerList
+    {
+        public List<Customer> customers;
+        private int count;
+
+        public CustomerList()
+        {
+            customers = null;
+            count = 0;
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public void Add(Customer c)
+        {
+            customers.Add(c);
+        }
+
+        public void Remove(Customer c)
+        {
+            customers.Remove(c);
+        }
+
+        static public CustomerList operator+(CustomerList cl, Customer c)
+        {
+            cl.Add(c);
+            return cl;
+        }
+
+        static public CustomerList operator-(CustomerList cl, Customer c)
+        {
+            cl.Remove(c);
+            return cl;
+        }
+    }
+}
